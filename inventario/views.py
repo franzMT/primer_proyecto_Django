906e5e06efe7e61f40e4ab54629b0836959012d1
franzbrayan76@gmail.com <franzbrayan76@gmail.com>
2023-06-26 00:29:52 -0300

    4ta actualizacion

--- inventario/views.py
+++ inventario/views.py
@@ -16,3 +16,27 @@ def crearProducto(request):
     producto=Productos(nombre=nombre,precio=precio,stock=stock)
     producto.save()
     return redirect("/")
+
+def eliminarProducto(request,id_producto):
+    producto = Productos.objects.get(id=id_producto)
+    producto.delete()
+    return redirect("/")
+
+def editarProducto(request,id_producto):
+    if request.method == "GET":
+        producto = Productos.objects.get(id=id_producto)
+        context = {"producto":producto}
+        return render(request,"editar.html",context)
+    elif request.method == "POST":
+        producto = Productos.objects.get(id=id_producto)
+        #capturo datos 
+        nuevo_nombre = request.POST["nombre"]
+        nuevo_precio = request.POST["precio"]
+        nuevo_stock = request.POST["stock"]
+        #asignar nuevo valor
+        producto.nombre = nuevo_nombre
+        producto.precio = nuevo_precio
+        producto.stock = nuevo_stock
+        #guardar
+        producto.save()
+        return redirect("/")
